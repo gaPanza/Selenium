9d1524254a198857129dc902f67744bcb4a008cf
G. Panza <mrpanza754@gmail.com>
2017-05-25 16:31:39 -0300

    Selenium Implementações para casa do biscoito

--- src/main/java/main/CasadoBiscoito.java
+++ src/main/java/main/CasadoBiscoito.java
@@ -1,12 +1,17 @@
 package main;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CasadoBiscoito {
 	private WebDriver driver;
@@ -17,25 +22,68 @@ public class CasadoBiscoito {
 
 		driver = new ChromeDriver();
 	}
-	
+
 	@Test
-	public void login() {
+	public void login() throws InterruptedException {
+
+		try {
+			driver.get("http://hmg.geoportal3d.com.br:8090/CasaDoBiscoitoTeste");
+			WebElement campoDeTexto = driver.findElement(By.name("formLogin:login"));
+			WebElement campoDeSenha = driver.findElement(By.name("formLogin:senha"));
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			campoDeTexto.sendKeys("adm");
+			campoDeSenha.click();
+			campoDeSenha.sendKeys("123456");
+			
+			WebElement botaoSalvar = driver.findElement(By.id("formLogin:j_idt20"));
+			botaoSalvar.click();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			WebElement negociacoes = driver.findElement(By.id("j_idt20:negociacao"));
+			negociacoes.click();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			
+			WebElement addNegociacoes = driver.findElement(By.id("j_idt27:novasNegociacoes"));
+			addNegociacoes.click();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			WebElement nome = driver.findElement(By.id("formNegociacao:nome"));
+			nome.sendKeys("Teste automatico Selenium");
+			WebElement data = driver.findElement(By.id("formNegociacao:data_input"));
+			data.sendKeys("24/06/2017");
+			WebElement hora = driver.findElement(By.id("formNegociacao:horaVigencia_input"));
+			hora.sendKeys("05:00");
 		
-		driver.get("http://localhost:8080/CasaDoBiscoito");
-		WebElement campoDeTexto = driver.findElement(By.name("formLogin:login"));
-		WebElement campoDeSenha = driver.findElement(By.name("formLogin:senha"));
-		campoDeTexto.sendKeys("adm");
-		campoDeSenha.click();
-		campoDeSenha.sendKeys("123456");
-		campoDeTexto.submit();
+			Actions actions = new Actions(driver);
+			
+			WebElement fabricantes = driver.findElement(By.id("formNegociacao:fabricante"));
+			actions.moveToElement(fabricantes);
+			actions.click();
+			actions.sendKeys("a",Keys.ENTER);
+			actions.build().perform();
+			
+					
+			
+			WebElement salvar = driver.findElement(By.id("formNegociacao:j_idt87"));
+			actions = new Actions(driver);
+			actions.moveToElement(salvar);
+			actions.click();
+			
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			addNegociacoes.click();
+			
+			
+			
+		}
 
-		WebElement botaoSalvar = driver.findElement(By.id("formLogin:j_idt20"));
-		botaoSalvar.click();
-
+		catch (org.openqa.selenium.StaleElementReferenceException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 	@After
-	public void encerra(){
+	public void encerra() {
 		driver.close();
 	}
 }
